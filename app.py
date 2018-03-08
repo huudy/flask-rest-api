f2f29f8ad380fec9575aa424dc034ecb908ca8b9
huudy <treendaa@gmail.com>
2018-03-08 23:01:17 +0100

    got it config final

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request
 from flask_restful import Api
 from flask_jwt import JWT
@@ -9,12 +10,12 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__, instance_relative_config=True)
 #app.config.from_object('config')
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 
-app.secret_key = app.config['SECRET_KEY']
+app.secret_key = os.environ.get('SECRET_KEY')
 api = Api(app)
 
 
