5ca17ce9612b8ce06cca89c22c7b0c71cf624577
huudy <treendaa@gmail.com>
2018-03-08 21:28:15 +0100

    app fixed to run app

--- app.py
+++ app.py
@@ -30,7 +30,4 @@ api.add_resource(Room, '/room/<string:name>')
 
 
 if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
-
-   # app.run(debug=True) # important to mention debug=True
+   app.run(debug=True) # important to mention debug=True
