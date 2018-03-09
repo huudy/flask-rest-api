7b03eed096be4c6c4aee72493a422c297e8da011
Trenda Paweł (UniCredit Business Integrated Solutions) <treendaa@gmail.com>
2018-03-09 11:38:01 +0100

    Room implementation cdn1

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from resources.room import Room, RoomList
+from resources.reservation import Reservation
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -32,6 +33,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Room, '/room/<string:name>')
 api.add_resource(RoomList, '/rooms')
+api.add_resource(Reservation, '/reserve')
 
 
 if __name__ == '__main__':
