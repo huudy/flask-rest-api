2353774996d12fdc6d800701335ddb8933820106
Trenda Paweł (UniCredit Business Integrated Solutions) <treendaa@gmail.com>
2018-03-22 16:01:33 +0100

    Small imporvements in reservation

--- resources/reservation.py
+++ resources/reservation.py
@@ -4,17 +4,25 @@ from security import login_required
 from models.reservation import ReservationModel
 from bson.json_util import dumps
 from bson.objectid import ObjectId
+from models.room import Rooms
 
 class Reservation(Resource):
     # @login_required
     def post(self):
         data = request.get_json()
        
-        reservations = db.rooms.find_one({
-            '_id':ObjectId(data['room_id'])}).aggregate([
-                {'$unwind' : { "$reserved" } },
-                {'$match': {'reserved':{ 'startDate': {'$lte': data['start_date'][:10] }, 'endDate': { '$gte':data['start_date'][:10] }}}}
-            ])
+        room = Rooms.objects.get(id=data['room_id'])
+        print(room)
+        
+        for res in room.reserved:
+            if data['start_date'] >= res.start_date and data['start_date'] <= res.end_date:
+                return {'message':'These date are already reserved for this room'}
+        
+        # db.rooms.find_one({
+        #     '_id':ObjectId(data['room_id'])}).aggregate([
+        #         {'$unwind' : { "$reserved" } },
+        #         {'$match': {'reserved':{ 'startDate': {'$lte': data['start_date'][:10] }, 'endDate': { '$gte':data['start_date'][:10] }}}}
+        #     ])
             
 
         print('Reservations: ', dumps(reservations))
