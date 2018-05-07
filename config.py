bb69abd7ab12f106809f62067d630fb61b31616a
Pablo <treendaa@gmail.com>
2018-05-07 17:08:42 +0200

    Security salt import os

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
+
 DEBUG = True # Turns on debugging features in Flask
 BCRYPT_LEVEL = 12 # Configuration for the Flask-Bcrypt extension
 MAIL_FROM_EMAIL = "robert@example.com" # For use in application emails
-
-SECURITY_PASSWORD_SALT = os.urandom(15)
